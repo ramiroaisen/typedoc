90340fc54fe8e8b9eb1f4aaad8e908e1528dcee1
ramiroaisen <ramiroaisen@gmail.com>
2021-11-20 21:37:39 -0300

    add folder icons

--- src/lib/output/themes/default/partials/navigation.tsx
+++ src/lib/output/themes/default/partials/navigation.tsx
@@ -15,7 +15,7 @@ export function navigation(context: DefaultThemeRenderContext, props: PageEvent<
 
 const folderIcon = () => {
     return (
-        <svg style="width:0.9em;height:0.9em;margin:0.2em 0 -0.2em 0" viewBox="0 0 24 24">
+        <svg style="width:0.9em;height:0.9em;margin:0.125em 0 -0.125em 0" viewBox="0 0 24 24">
             <path fill="currentColor" d="M6.1,10L4,18V8H21A2,2 0 0,0 19,6H12L10,4H4A2,2 0 0,0 2,6V18A2,2 0 0,0 4,20H19C19.9,20 20.7,19.4 20.9,18.5L23.2,10H6.1M19,18H6L7.6,12H20.6L19,18Z" />
         </svg>
     )
